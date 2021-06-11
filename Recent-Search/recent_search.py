edef9c0c2394da502720f913f20e8bb10fcae698
Aysu Yaman <50742940+aysuyaman@users.noreply.github.com>
2021-06-12 00:35:22 +0300

    Update recent_search.py

--- Recent-Search/recent_search.py
+++ Recent-Search/recent_search.py
@@ -4,7 +4,7 @@ import json
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = os.environ.get("BEARER_TOKEN")
+bearer_token = AAAAAAAAAAAAAAAAAAAAAIj9PwEAAAAA9y3WDFdIMPni%2BtWM16gN7nYTQ6Q%3DVCcYlo3RFoWfqf9e5BDOtJpgR2Xfdg8K8j2uI4eBO7rJqjUcyS
 
 search_url = "https://api.twitter.com/2/tweets/search/recent"
 
